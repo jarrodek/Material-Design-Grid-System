7fb1e6c7a577d05a4699c6597fe6c52f554cbd14
Paweł Psztyć <jarrodek@gmail.com>
2015-08-12 17:34:37 +0100

    Created project. Signed-off-by:Paweł Psztyć <jarrodek@gmail.com>

---
+++ wct.conf.js
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2015 Pawel Psztyc <jarrodek@gmail.com>. All rights reserved.
+ */
+
+
+module.exports = {
+  verbose: true,
+  plugins: {
+    local: {
+      browsers: ['chrome', 'firefox']
+    }
+  }
+};
